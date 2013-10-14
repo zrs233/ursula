a3501deb5f28b75aac23f08174386734ac61d462
John Dewey <john@dewey.ws>
2013-10-14 10:27:35 -0700

    Disable memcached
    We want to disable memcached until we patch availability_zones.py [1]
    
    [1] https://review.openstack.org/#/c/41873/

--- test/integration/test_nova_common.py
+++ test/integration/test_nova_common.py
@@ -10,6 +10,6 @@ import unittest_helper as helper
 
 class TestNovaCommon(unittest.TestCase):
     def test_contains_a_memcached_servers_flag(self):
-        cmd = ('egrep memcached_servers=[0-9.]+:11211,[0-9.]+ '
+        cmd = ('egrep \\#memcached_servers=[0-9.]+:11211,[0-9.]+ '
                '/etc/nova/nova.conf')
         helper.run_on_group('controller', cmd)
