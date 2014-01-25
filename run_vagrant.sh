82c806a7dc8a1c5460263aa256892dd703ff2924
Craig Tracey <craigtracey@gmail.com>
2014-01-24 19:53:17 -0500

    Cleanup variables for run_vagrant
    Clean up variables and add a new "standard" provisioning type.

--- run_vagrant.sh
+++ run_vagrant.sh
@@ -9,25 +9,27 @@ function get_vagrant_hosts {
 
 TYPE=$1
 
-if [ -z $TYPE ]; then
+if [ -z "${TYPE}" ]; then
     echo "You must specify a type"
     exit -1
 fi
 
-if [ "$TYPE" == "swift" ]; then
+if [ "${TYPE}" == "swift" ]; then
     BOXES="controller1 swiftnode1 swiftnode2 swiftnode3"
+elif [ "${TYPE}" == "standard" ]; then
+    BOXES="controller1 controller2 compute1"
 else
-    echo "I dont know how to provision '$TYPE'"
+    echo "I dont know how to provision '${TYPE}'"
     exit -1
 fi
 
-vagrant up --no-provision $BOXES
+vagrant up --no-provision ${BOXES}
 
 TEMPFILE=`mktemp`
-for I in $BOXES; do
-    vagrant ssh-config $I >> $TEMPFILE
+for I in ${BOXES}; do
+    vagrant ssh-config $I >> ${TEMPFILE}
 done
 
 export ANSIBLE_SSH_ARGS="${ANSIBLE_SSH_ARGS} -F ${TEMPFILE}"
-./bin/ursula envs/vagrant/$TYPE site.yml -s -u vagrant
-rm $TEMPFILE
+./bin/ursula envs/vagrant/${TYPE} site.yml -s -u vagrant
+rm ${TEMPFILE}
