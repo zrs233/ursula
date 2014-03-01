ecbeb03059304913091fabee2a879c3b8f9fe648
Sam Cooper <scooper@bluebox.net>
2014-02-28 16:41:47 -0800

    refactor to get rid of top_process

--- playbooks/monitoring/files/sensu_plugins/check-cpu.rb
+++ playbooks/monitoring/files/sensu_plugins/check-cpu.rb
@@ -84,14 +84,11 @@ class CheckCPU < Sensu::Plugin::Check::CLI
     message msg
 
     if checked_usage > config[:crit] || checked_usage > config[:warn]
-      top_process = get_top_process_by_cpu_mem
+      unless process_in_white_list?(get_top_process_by_cpu_mem)
+        critical if checked_usage > config[:crit] 
+        warning if checked_usage > config[:warn]
+      end
     end
-
-    unless process_in_white_list?(top_process)
-      critical if checked_usage > config[:crit] 
-      warning if checked_usage > config[:warn]
-    end
-    
     ok
   end
 
