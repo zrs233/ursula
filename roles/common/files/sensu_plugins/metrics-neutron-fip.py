bead0338881415905df16897f929562fd77cbe3b
paulczar <username.taken@gmail.com>
2015-01-20 15:40:03 -0600

    neutron fip checks and metrics

---
+++ roles/common/files/sensu_plugins/metrics-neutron-fip.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+#
+# Neutron FIP usage metrics
+# ===
+#
+# Dependencies
+# -----------
+# - python-neutronclient and related libraries
+#
+# Performs API query to get stats for neutron
+# floating IPs
+#
+# Author: Paul Czarkowski (pczarkowski@bluebox.net)
+# Significantly based on existing openstack checks found
+# in https://github.com/sensu/sensu-community-plugins
+#
+# Released under the same terms as Sensu (the MIT license);
+# see LICENSE for details.
+#
+
+# #RED
+import sys
+import argparse
+import logging
+import os
+import socket
+import time
+
+from neutronclient.neutron import client
+
+STATE_OK = 0
+STATE_WARNING = 1
+STATE_CRITICAL = 2
+STATE_UNKNOWN = 3
+
+
+parser = argparse.ArgumentParser(description='Check OpenStack Neutron API status')
+
+parser.add_argument('-u', '--user', default=os.environ['OS_USERNAME'])
+parser.add_argument('-p', '--password', default=os.environ['OS_PASSWORD'])
+parser.add_argument('-t', '--tenant', default=os.environ['OS_TENANT_NAME'])
+parser.add_argument('-a', '--auth-url', default=os.environ['OS_AUTH_URL'])
+parser.add_argument('-s', '--scheme', default=socket.gethostname())
+args = parser.parse_args()
+
+try:
+    c = client.Client('2.0',
+                      username=args.user,
+                      tenant_name=args.tenant,
+                      password=args.password,
+                      auth_url=args.auth_url)
+    fips_obj = c.list_floatingips()
+    fips = fips_obj['floatingips']
+    count_used_fips = 0
+    count_free_fips = 0
+    for fip in fips:
+        if fip['fixed_ip_address']:
+            count_used_fips += 1
+        else:
+            count_free_fips += 1
+
+
+except Exception as e:
+    print str(e)
+    sys.exit(STATE_CRITICAL)
+
+print "{scheme}.fip.free {free} {timestamp}".format(scheme=args.scheme, free=count_free_fips, timestamp=int(time.time()))
+print "{scheme}.fip.used {used} {timestamp}".format(scheme=args.scheme, used=count_used_fips, timestamp=int(time.time()))
+print "{scheme}.fip.total {total} {timestamp}".format(scheme=args.scheme, total=len(fips), timestamp=int(time.time()))
+
+
+sys.exit(STATE_OK)
