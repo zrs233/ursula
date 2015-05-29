f09311687781bf7697fde89e2e3c660077e3b016
Dustin Lundquist <dustin@null-ptr.net>
2015-05-29 15:31:50 -0700

    Combine Ursula filters
    Combine all Ursula filters into a single file.

--- plugins/filters/ursula_filters.py
+++ plugins/filters/ursula_filters.py
@@ -1,3 +1,6 @@
+import os
+
+
 def ursula_controller_ips(hostvars, groups, controller_name='controller'):
     controller_ips = set()
     for host in groups[controller_name]:
@@ -5,7 +8,7 @@ def ursula_controller_ips(hostvars, groups, controller_name='controller'):
         ip = hostvars[host][controller_primary_interface]['ipv4']['address']
         controller_ips.add(ip)
     return sorted(list(controller_ips))
-       
+
 
 def ursula_memcache_hosts(hostvars, groups, memcache_port, controller_name='controller'):
     controller_ips = ursula_controller_ips(hostvars, groups, controller_name)
@@ -13,11 +16,18 @@ def ursula_memcache_hosts(hostvars, groups, memcache_port, controller_name='cont
     return ','.join(host_strings)
 
 
+def ursula_package_path(project, version):
+    package_name_format = "openstack-%(version)s" % locals()
+    path = os.path.join("/opt/bbc", package_name_format, project)
+    return path
+
+
 class FilterModule(object):
     ''' ursula utility filters '''
 
     def filters(self):
         return {
-            'ursula_controller_ips': ursula_controller_ips,
-            'ursula_memcache_hosts': ursula_memcache_hosts
+            'ursula_controller_ips':    ursula_controller_ips,
+            'ursula_memcache_hosts':    ursula_memcache_hosts,
+            'ursula_package_path':      ursula_package_path,
         }
