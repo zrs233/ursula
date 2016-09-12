a637af4a065c097091e7f80ced41bc73441d3439
Nikhil Gupta <lihkin213@users.noreply.github.com>
2016-09-12 18:22:16 -0500

    Add HTTPOnly attribute to CSRF and Session cookies to prevent access to (#2164)
    the cookies from JavaScripts in case of a XSS vulnerability

--- roles/horizon/templates/etc/openstack-dashboard/local_settings.py
+++ roles/horizon/templates/etc/openstack-dashboard/local_settings.py
@@ -37,6 +37,8 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTOCOL', 'https')
 # settings to better secure the cookies from security exploits
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_HTTPONLY = True
+SESSION_COOKIE_HTTPONLY = True
 SESSION_ENGINE = "{{ horizon.session_engine }}"
 
 # The absolute path to the directory where message files are collected.
